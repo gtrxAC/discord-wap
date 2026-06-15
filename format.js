538c3805a2be1e969de6b421285bcc40c66d3ca1
gtrxAC <gtrxac2@gmail.com>
2026-06-15 23:15:48 +0300

    Improve string truncation

--- format.js
+++ format.js
@@ -32,7 +32,10 @@ function getCharactersPerLine(req) {
     // models with 96×65 or similar display (list may be incomplete)
     if (/^Nokia(1101|3350|3410|35[^0]\d|3610|6010|6210|6310|6510|7110|8910)/.test(ua)) return 19;
 
-    // other nokias, assume a 128×128 or 128×160 display
+    // other nokias when in WML mode, assume older WML-only browser with larger font size, e.g. Nokia 6100
+    if (res.locals.format == "wml") return 16;
+
+    // other nokias when in HTML mode, assume a 128×128 or 128×160 display with smaller font
     return 21;
 }
 
@@ -86,7 +89,7 @@ function oneLine(req, res, str, charsUsed = 0) {
 
     if (!res.locals.theme.oneLineTruncate) return str;
 
-    const chars = getCharactersPerLine(req) - charsUsed
+    const chars = getCharactersPerLine(req) - charsUsed;
 
     if (str.length > chars) return str.substring(0, chars - 1).trimEnd() + "...";
     return str;
